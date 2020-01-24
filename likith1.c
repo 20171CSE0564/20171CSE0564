12c3c7c37d67991e5ed366e15e4a84fb6d8cc002
20171CSE0564 <60248941+20171CSE0564@users.noreply.github.com>
2020-01-24 10:55:08 +0530

    Add files via upload

---
+++ likith1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+   int a,b;
+   printf("Enter a and b value\n");
+   scanf("%d%d",&a,&b);
+   if(a==b)
+   {
+     printf("a=%d and b=%d are equal\n",a,b);
+    }
+
+}
